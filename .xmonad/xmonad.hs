f10c6b58bda88c11b8fbcb783baa6c21305947c5
folone <folone@gmail.com>
2013-01-29 12:42:45 +0100

    Initial commit

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,94 @@
+-- Imports.
+import qualified Data.Map        as M
+import Data.Ratio
+
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.EZConfig(additionalKeys, additionalKeysP)
+import XMonad.Actions.CycleWS
+import XMonad.Layout.Combo
+import XMonad.Layout.Grid
+import XMonad.Layout.LayoutModifier
+import XMonad.Layout.Named
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.Reflect
+import XMonad.Layout.TwoPane
+import XMonad.Layout.WindowNavigation
+import XMonad.Layout.Circle
+import XMonad.Layout.MosaicAlt
+import XMonad.Layout.Spiral
+import DBus.Client
+import System.Taffybar.XMonadLog ( dbusLogWithPP, taffybarDefaultPP, taffybarColor, taffybarEscape )
+import qualified Debug.Trace as D
+
+-- The main function.
+main = do
+  client <- connectSession
+  let pp = myTaffyBarPP
+  xmonad $ myConfig { logHook = dbusLogWithPP client pp }
+
+-- Custom PP.
+myTaffyBarPP = taffybarDefaultPP {
+                            ppCurrent = taffybarColor "#f8f8f8" "DodgerBlue4"   . wrap " " " "
+                          , ppVisible = taffybarColor "#f8f8f8" "LightSkyBlue4" . wrap " " " "
+                          , ppUrgent  = taffybarColor "#f8f8f8" "red4"          . wrap " " " "
+                          , ppLayout  = taffybarColor "DarkOrange" "" . wrap " [" "] "
+                          , ppTitle   = taffybarColor "#61ce3c" "" . shorten 50
+  }
+
+-- Keybinding to toggle the gap for the bar.
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+altMask    = mod1Mask
+myModMask  = mod4Mask
+myTerminal = "urxvt"
+
+myWorkSpaces = ["α", "β", "γ", "δ", "ε", "ζ", "η", "θ", "ι"]
+
+-- Layouts
+basicLayout = Tall nmaster delta ratio where
+    nmaster = 1
+    delta   = 3/100
+    ratio   = 1/2
+tallLayout       = named "tall"     $ avoidStruts $ basicLayout
+wideLayout       = named "wide"     $ avoidStruts $ Mirror basicLayout
+singleLayout     = named "single"   $ avoidStruts $ noBorders Full
+circleLayout     = named "circle"   $ Circle
+twoPaneLayout    = named "two pane" $ TwoPane (2/100) (1/2)
+mosaicLayout     = named "mosaic"   $ MosaicAlt M.empty
+gridLayout       = named "grid"     $ Grid
+spiralLayout     = named "spiral"   $ spiral (1 % 1)
+
+myLayoutHook = smartBorders $ tallLayout ||| wideLayout   ||| singleLayout ||| circleLayout
+                                         ||| mosaicLayout ||| gridLayout   ||| spiralLayout
+
+-- Main config.
+myConfig = defaultConfig {
+    modMask            = myModMask
+  , terminal           = myTerminal
+  , focusFollowsMouse  = False
+  , workspaces         = myWorkSpaces
+  , layoutHook         = myLayoutHook
+  , manageHook         = manageHook defaultConfig <+> manageDocks
+  , normalBorderColor  = "#2a2b2f"
+  , focusedBorderColor = "DarkOrange"
+  , borderWidth        = 1
+  , startupHook        = spawn "taffybar"
+  } `additionalKeysP`
+        [ ("<XF86AudioRaiseVolume>", spawn "/home/folone/bin/volume_level.sh up")   -- volume up
+        , ("<XF86AudioLowerVolume>", spawn "/home/folone/bin/volume_level.sh down") -- volume down
+        , ("<XF86AudioMute>"       , spawn "/home/folone/bin/volume_level.sh mute") -- mute
+        , ("<XF86Launch1>"         , spawn "dmenu_run")                             -- dmenu
+        , ("<XF68ScreenSaver>"     , spawn "xlock")                                 -- lock screen
+        ]
+   `additionalKeys`
+        [ ((controlMask .|. altMask, xK_l), spawn "xlock")                          -- lock screen
+        , ((controlMask, xK_Print)        , spawn "sleep 0.2; scrot -s")            -- screenshot
+        , ((0, xK_Print)                  , spawn "scrot")                          -- screenshot
+        , ((mod4Mask, xK_p)               , spawn "dmenu_run")                      -- dmenu
+        , ((mod4Mask, xK_Left  ), prevWS)
+        , ((mod4Mask, xK_Right ), nextWS)
+        , ((mod4Mask, xK_q)               , spawn "/home/folone/bin/xmonad-restart.sh")
+        ]
---
+++ .zshrc
@@ -0,0 +1,42 @@
+# Path to your oh-my-zsh configuration.
+ZSH=$HOME/.oh-my-zsh
+
+# Set name of the theme to load.
+# Look in ~/.oh-my-zsh/themes/
+# Optionally, if you set this to "random", it'll load a random theme each
+# time that oh-my-zsh is loaded.
+ZSH_THEME="kolo"
+
+RPROMPT='%{%B%F{green}%}%~%{%f%k%b%}'
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+
+# Set to this to use case-sensitive completion
+# CASE_SENSITIVE="true"
+
+# Comment this out to disable bi-weekly auto-update checks
+# DISABLE_AUTO_UPDATE="true"
+
+# Uncomment to change how many often would you like to wait before auto-updates occur? (in days)
+# export UPDATE_ZSH_DAYS=13
+
+# Uncomment following line if you want to disable colors in ls
+# DISABLE_LS_COLORS="true"
+
+# Uncomment following line if you want to disable autosetting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment following line if you want red dots to be displayed while waiting for completion
+# COMPLETION_WAITING_DOTS="true"
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+plugins=(git)
+
+source $ZSH/oh-my-zsh.sh
+
+# Customize to your needs...
+export SCALA_HOME=/usr/bin/scala/
+export PATH=/home/folone/perl5/bin:/home/folone/perl5/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/bin/core_perl:/opt/qt/bin:/opt/maven/bin/:/home/folone/.cabal/bin:/home/folone/node_modules/roy:/home/folone/bin/:/home/folone/bin/play/:/opt/maven/bin/:/home/folone/.cabal/bin:/home/folone/node_modules/roy:/home/folone/bin/:/home/folone/bin/play/:$SCALA_HOME
