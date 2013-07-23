65d3848960778b6397fbddb6731ce651fb9de9eb
folone <folone@gmail.com>
2013-07-23 11:08:37 +0200

    Removing binary.

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 sudo pacman -Syu
-sudo pacmad -S zsh xmonad xmonad-contrib yaourt sbt tig
+sudo pacmad -S zsh xmonad xmonad-contrib yaourt sbt tig cabal-install
 yaourt -S taffybar-git
+cabal update
+cabal install stylish-haskell hlint
