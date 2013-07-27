6baf78281b608b03849717178b0a5ebc3700e620
folone <folone@gmail.com>
2013-07-27 16:12:46 +0200

    Some touches

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo pacman -Syu
-sudo pacmad -S zsh xmonad xmonad-contrib yaourt sbt tig cabal-install
-yaourt -S taffybar-git
+sudo pacmad -S zsh xmonad xmonad-contrib yaourt sbt tig cabal-install trayer
+yaourt -S taffybar-git volume-applet
 cabal update
 cabal install stylish-haskell hlint
