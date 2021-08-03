7d1d7509b228df5f5c5992737ffbc15c890955f1
George Leontiev <gleontiev@twitter.com>
2021-08-03 11:33:32 +0100

    .

--- install.sh
+++
@@ -1,5 +0,0 @@
-sudo pacman -Syu
-sudo pacmad -S zsh xmonad xmonad-contrib yaourt sbt tig cabal-install trayer
-yaourt -S taffybar-git volume-applet emacs-gtk2 hott
-cabal update
-cabal install stylish-haskell hlint
