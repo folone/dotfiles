75d358dee99d6ec490ff36fccf9478ef43bf71cf
George Leontiev <gleontiev@twitter.com>
2021-08-06 21:26:09 +0100

    Add configs.

--- install.sh
+++ install.sh
@@ -10,6 +10,14 @@ brew upgrade
 # Install all the packages
 brew install rg htop tig antigen oh-my-zsh gnupg cmacrae/formulae/spacebar koekeishiya/formulae/yabai
 
+# Iterm2
+brew install --cask iterm2
+
+# Onivim2 editor
+brew tap marblenix/onivim2
+brew install --cask onivim2
+
+
 # Install vim pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
