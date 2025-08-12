776b10778808800f63ddd90ecc6d764ff502323b
George Leontiev <george.leontiev@reddit.com>
2025-08-12 12:08:50 +0100

    shellcheck

--- .config/sketchybar/scripts/wifi.sh
+++ .config/sketchybar/scripts/wifi.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-SSID=$(networksetup -getairportnetwork en0 2>/dev/null | sed 's/^Current Wi-Fi Network: //')
+# Clear label (icon only)
 sketchybar --set "${NAME:-wifi}" label=""
